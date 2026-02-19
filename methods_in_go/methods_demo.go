c71b57fe6095b3b336a929134733655baec33c36
Vishwajeet Singh <vishwajeetsingh.sde@gmail.com>
2026-02-19 19:42:40 +0530

    methods on non-struct types

--- methods_in_go/methods_demo.go
+++ methods_in_go/methods_demo.go
@@ -18,9 +18,22 @@ func (s Square) Diagonal() float64 {
 	return s.side * 1.41421356237
 }
 
+// we can declare methods on non-struct types as well
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
 func main() {
 	s := Square{side: 5}
 	fmt.Printf("Area: %f\n", s.Area())
 	fmt.Printf("Perimeter: %f\n", s.Perimeter())
 	fmt.Printf("Diagonal: %f\n", s.Diagonal())
+
+	f := MyFloat(-3.14)
+	fmt.Printf("Absolute value: %f\n", f.Abs())
 }
