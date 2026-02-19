dcba0de903f420b061fe8370cd7d347b87bc610f
iot-with-vsr <123094039+iot-with-vsr@users.noreply.github.com>
2026-02-19 13:04:17 +0530

    slice literals

--- slices_in_go/slices_demo.go
+++ slices_in_go/slices_demo.go
@@ -23,4 +23,26 @@ func main() {
 	sliceFromArr[0] = 25 // This modifies the first element of the slice, which is also the second element of the original array
 	fmt.Println("Modified slice from array:", sliceFromArr)
 	fmt.Println("Original array after modification:", arr)
+
+	//slice literals
+
+	q := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println(q)
+
+	r := []bool{true, false, true, true, false, true}
+	fmt.Println(r)
+
+	s := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},
+		{13, true},
+	}
+	fmt.Println(s)
+
 }
