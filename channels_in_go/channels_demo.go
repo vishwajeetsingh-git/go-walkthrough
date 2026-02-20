a9bed3bf51197abddd3f31a234961c8b07f8ee59
Vishwajeet Singh <vishwajeetsingh.sde@gmail.com>
2026-02-20 20:10:08 +0530

    channels can be closed

--- channels_in_go/channels_demo.go
+++ channels_in_go/channels_demo.go
@@ -5,13 +5,25 @@ import (
 	"time"
 )
 
+// recives a channel and a number n 
+// computes the first n numbers in the fibonacci 
+// sequence and sends them to the channel 
+func fibonacci(n int, ch chan int) {
+	a, b := 0, 1
+	for i := 0; i < n; i++ {
+		ch <- a
+		a, b = b, a+b
+	}
+	close(ch)
+}
+
 func main() {
 	fmt.Println("Goroutines Channels Demo")
 
 	ch := make(chan string)
 
 	go func() {
-		time.Sleep(2 * time.Second)
+		time.Sleep(500 * time.Millisecond)
 		ch <- "Hello from Goroutine"
 	}()
 
@@ -29,4 +41,19 @@ func main() {
 	fmt.Println("Messages in buffered channel:")
 	fmt.Println(<-bufferedCh)
 	fmt.Println(<-bufferedCh)
+
+	// channels can be closed by the sender 
+	// to indicate that no more vaues will be 
+	// sent 
+
+	ch2 := make(chan int)
+	go fibonacci(10, ch2)
+
+	fmt.Println("Fibonacci sequence:")
+
+	// range over the channel to receive values
+	//  until it is closed
+	for num := range ch2 {
+		fmt.Println(num)
+	}
 }
